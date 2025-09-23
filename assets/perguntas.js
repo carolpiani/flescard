93758f9ff521049da4f022c5037aa2e41da48368
carolpiani <piani.carolina@escola.pr.gov.br>
2025-09-23 12:16:20 -0300

    ...

--- assets/perguntas.js
+++ assets/perguntas.js
@@ -1,69 +1,69 @@
 criaCartao(
-    'Programação',
+    'Conhecimentos Gerais',
     'Quantas copas o Brasil já conquistou?',
     'O Brasil ganhou 5 copas do mundo'
 )
 
 criaCartao(
-    'Geografia',
+    'Conhecimentos Gerais',
     'Qual ano Brasil ganhou o penta?',
     'O Brasil ganhou o penta em 2002'
 )
 
 criaCartao(
-    'Programação',
+    'Automobilimo',
     'Quem foi o maior ídolo do automobilismo no Brasil?'
     ' Ayrton Senna'
 )
 
 criaCartao(
-    'Lingua inglesa',
+    'Geografio',
     ' Qual monumento mais famoso pela sua inclinação?',
     ' Torre de pisa'
 
 )criaCartao(
-    'Programação',
+    'Futebol',
     'Qual o esporte mais popular do Brasil?',
     'Futebol'
 )
 
 criaCartao(
-    'Geografia',
+    'Conhecimentos Gerais',
     'Qual o nome do atual presidente do Brasil?',
     ' Luiz Inácio Lula da Silva'
 )
 
 criaCartao(
-    'Programação',
+    'Aviação',
     ' Quem é coniderado o pai da aviação?',
     'Santos Dumont'
 )
 
 criaCartao(
-    'Lingua inglesa',
+    'Conhecimentos Gerais',
     'Qual famoso navio afundou na ua viagem inaugural?',
     ' RMS Titanic'
 
 )criaCartao(
-    'Programação',
+    'Geografia',
     ' Qual a maior floresta do mundo?',
     ' Floresta Amazônica'
 )
 
 criaCartao(
-    'Geografia',
+    'Quimíca',
     'Quantos elementos tem na tabela periódica?',
     ' 118 elementos'
 )
 
 criaCartao(
-    'Programação',
+    'Geografia',
     ' Qual satélite natural da Terra?',
     'Lua'
 )
 
 criaCartao(
-    'Lingua inglesa',
+    'Igreja',
     'ual nome do antigo Papa da igreja católica?,
     'Papa Francico'
 )

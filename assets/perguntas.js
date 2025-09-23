462101b7fd80a9ac96981260aead768ffb6ca93b
carolpiani <piani.carolina@escola.pr.gov.br>
2025-09-23 12:11:20 -0300

    ..

---
+++ assets/perguntas.js
@@ -0,0 +1,69 @@
+criaCartao(
+    'Programação',
+    'Quantas copas o Brasil já conquistou?',
+    'O Brasil ganhou 5 copas do mundo'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual ano Brasil ganhou o penta?',
+    'O Brasil ganhou o penta em 2002'
+)
+
+criaCartao(
+    'Programação',
+    'Quem foi o maior ídolo do automobilismo no Brasil?'
+    ' Ayrton Senna'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    ' Qual monumento mais famoso pela sua inclinação?',
+    ' Torre de pisa'
+
+)criaCartao(
+    'Programação',
+    'Qual o esporte mais popular do Brasil?',
+    'Futebol'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o nome do atual presidente do Brasil?',
+    ' Luiz Inácio Lula da Silva'
+)
+
+criaCartao(
+    'Programação',
+    ' Quem é coniderado o pai da aviação?',
+    'Santos Dumont'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Qual famoso navio afundou na ua viagem inaugural?',
+    ' RMS Titanic'
+
+)criaCartao(
+    'Programação',
+    ' Qual a maior floresta do mundo?',
+    ' Floresta Amazônica'
+)
+
+criaCartao(
+    'Geografia',
+    'Quantos elementos tem na tabela periódica?',
+    ' 118 elementos'
+)
+
+criaCartao(
+    'Programação',
+    ' Qual satélite natural da Terra?',
+    'Lua'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'ual nome do antigo Papa da igreja católica?,
+    'Papa Francico'
+)
